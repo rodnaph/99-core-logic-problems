d0ce01ca769aadfec12de52532e6f763ab41ac16
rod <rod@pu-gh.com>
2013-02-04 23:19:42 +0000

    last but one element of list

--- src/problems/lists.clj
+++ src/problems/lists.clj
@@ -3,11 +3,20 @@
   (:refer-clojure :exclude [==])
   (:use clojure.core.logic))
 
-; 1.01
+; 1.01 - Last element of a list
 
 (defne my-lasto
   [xs x]
   ([[a . ()] _] (== a x))
   ([[_ . a] _]
     (my-lasto a x)))
+
+; 1.02 - Last but one element of a list
+
+(defne my-lastbutoneo
+  [xs x]
+  ([[a . [_ . ()]] _]
+   (== a x))
+  ([[_ . a] _]
+   (my-lastbutoneo a x)))
 
